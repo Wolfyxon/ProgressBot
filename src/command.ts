ed7dba36859bca85c71150a7c531594c635a0fcd
Wolfyxon <wolfyxon@gmail.com>
2025-02-11 15:34:33 +0100

    Added getCommands()

--- src/command.ts
+++ src/command.ts
@@ -1,9 +1,12 @@
 import { CommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from "discord.js";
+import * as fs from "fs";
 
 type CommandData = {
     builder: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder,
     run: (interaction: CommandInteraction) => void
 };
+
+const COMMAND_DIR = "src/commands";
 
 export default class Command {
     public data: CommandData
@@ -11,4 +14,12 @@ export default class Command {
     constructor(data: CommandData) {
         this.data = data;
     }
+}
+
+export async function getCommands(): Promise<Command[]> {
+    return await Promise.all(fs.readdirSync(COMMAND_DIR).map(async (file: string) => {
+        const module = await import(`../${COMMAND_DIR}/${file}`);
+
+        return module.default as Command;
+    }));
 }
