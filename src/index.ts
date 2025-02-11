a4274bec755919d8981d9b14b3e23bd3a5bb8da4
Wolfyxon <wolfyxon@gmail.com>
2025-02-11 16:07:21 +0100

    Command handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { getCommands } from "./command";
 
 console.log("Welcome to ProgressBot");
 console.log("https://github.com/Wolfyxon/ProgressBot");
@@ -8,14 +9,32 @@ dotenv.config();
 
 const TOKEN: string = process.env["DISCORD_TOKEN"]!;
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds
-    ]
-});
+async function main() {
+    const commands = await getCommands();
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds
+        ]
+    });
+    
+    
+    
+    client.once(Events.ClientReady, () => {
+        console.log(`Logged in successfully as ${client.user?.tag}`);
+    });
 
-client.once(Events.ClientReady, () => {
-    console.log(`Logged in successfully as ${client.user?.tag}`);
-});
+    client.on(Events.InteractionCreate, interaction => {
+        if (!interaction.isChatInputCommand()) return;
 
-client.login(TOKEN);
+        for(const cmd of commands) {
+            if(cmd.data.builder.name == interaction.commandName) {
+                cmd.execute(interaction);
+                break;
+            }
+        }
+    })
+    
+    client.login(TOKEN);
+}
+
+main();
