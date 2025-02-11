35283107487e72f8c4abf734cf8103a0ec7475d9
Wolfyxon <wolfyxon@gmail.com>
2025-02-11 22:54:27 +0100

    Added setup

--- src/commands/xp.ts
+++ src/commands/xp.ts
@@ -23,6 +23,11 @@ export default new Command({
                             .setDescription("Amount of XP to add")
                             .setRequired(true)
                     )
+            )
+            .addSubcommand(cmd => 
+                cmd
+                    .setName("setup")
+                    .setDescription("Sets up all users in the server") 
             ),
     
     run: async (ctx) => {
@@ -32,7 +37,7 @@ export default new Command({
             case "add": {
                 const amount = ctx.interaction.options.getNumber("amount", true);
                 const user = ctx.interaction.options.getUser("user", true);
-                
+
                 const dbUser = ctx.db.getOrTemplateGuildUser(ctx.interaction.guildId!, user.id);
                 dbUser.xp += amount;
 
@@ -40,6 +45,29 @@ export default new Command({
 
                 ctx.interaction.editReply(`Added ${amount} XP to ${ctx.interaction.user.displayName}`);
 
+                break;
+            }
+
+            case "setup": {
+                const limit = 100;
+
+                if(ctx.interaction.guild?.memberCount! > limit) {
+                    ctx.interaction.editReply("Too much members. Edit the code if you want to remove this limit.");
+                    return;
+                }
+
+                const members = (await ctx.interaction.guild?.members.fetch())!;
+
+                if(members.size == 0) {
+                    ctx.interaction.reply("No users found, please enable the guild memebers intent");
+                    return;
+                }
+
+                members.forEach((member) => {
+                    ctx.db.setupUser(ctx.interaction.guildId!, member.id);
+                });
+
+                ctx.interaction.editReply("Users added");
                 break;
             }
 
