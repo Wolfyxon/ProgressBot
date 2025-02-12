86fa3ec4493825573966674fb1fb100d1e31881e
Wolfyxon <wolfyxon@gmail.com>
2025-02-12 13:07:19 +0100

    Init rank command

---
+++ src/commands/rank.ts
@@ -0,0 +1,44 @@
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import Command from "../command";
+
+export default new Command({
+    builder: new SlashCommandBuilder()
+            .setName("rank")
+            .setDescription("Checks your or someone else's rank")
+            
+            .addUserOption(option => option
+                .setName("user")
+                .setDescription("User to check the rank of")
+                .setRequired(false)
+                
+            ),
+    
+    run: async (ctx) => {        
+        await ctx.interaction.deferReply();
+
+        const user = ctx.interaction.options.getUser("user") ?? ctx.interaction.user;
+        const dbUser = ctx.db.getOrTemplateGuildUser(ctx.interaction.guildId!, ctx.interaction.user.id);
+
+        const xp = dbUser.getLevelXp();
+        const reqXp = dbUser.getXpForNextLevel();
+
+        const barWidth = 16;
+        const dispXp = Math.floor(barWidth * (xp / reqXp))
+
+        let bar = "🟩".repeat(dispXp) + "⬛".repeat(barWidth - dispXp);
+
+        const embed = new EmbedBuilder()
+                        .setAuthor({
+                            name: user.displayName,
+                            iconURL: user.avatar ?? ""
+                        })
+                        .setTitle(`Level ${dbUser.getLevel()}`)
+                        .setThumbnail(user.avatar)
+                        .setDescription(`${bar} ${xp}/${reqXp}`)
+                        .setFooter({
+                            text: `@${user.tag} | ${user.id}`
+                        })
+
+        ctx.interaction.editReply({embeds: [embed]});
+    }
+});
