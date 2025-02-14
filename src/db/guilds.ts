7bf6158c6261aa4b4ada1fcf7f3c69ad146c0878
Wolfyxon <wolfyxon@gmail.com>
2025-02-14 10:31:25 +0100

    Added language column

--- src/db/guilds.ts
+++ src/db/guilds.ts
@@ -16,6 +16,7 @@ export default class Guilds extends DbTable {
             CREATE TABLE IF NOT EXISTS ${this.name} (
                 guildId VARCHAR(20) NOT NULL UNIQUE,
                 teacherRoleId VARCHAR(20),
+                language VARCHAR(5) DEFAULT "en"
             )
         `);
     }
