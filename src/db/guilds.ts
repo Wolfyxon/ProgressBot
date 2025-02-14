6cafd4331501dca8cd4ab2bd5497cbad536bdbaa
Wolfyxon <wolfyxon@gmail.com>
2025-02-14 10:28:03 +0100

    Init guilds table

---
+++ src/db/guilds.ts
@@ -0,0 +1,22 @@
+import Database from "./database";
+import DbTable from "./table";
+
+export type RawDbGuild = {
+    guildId: string,
+    teacherRoleId?: string
+}
+
+export default class Guilds extends DbTable {
+    constructor(db: Database) {
+        super("Guilds", db);
+    }
+
+    public setup(): void {
+        this.db.db.exec(`
+            CREATE TABLE IF NOT EXISTS ${this.name} (
+                guildId VARCHAR(20) NOT NULL UNIQUE,
+                teacherRoleId VARCHAR(20),
+            )
+        `);
+    }
+}
