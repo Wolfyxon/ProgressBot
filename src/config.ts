53977f371e8bf2ef781e8efd76c3f9efa71dbc72
Wolfyxon <wolfyxon@gmail.com>
2025-04-09 22:31:11 +0200

    Init Config

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+
+const CONFIG_PATH = "config.json";
+
+export type ConfigData = {
+    developers: string[]
+}
+
+export class Config {
+    data: ConfigData;
+
+    constructor(data: ConfigData) {
+        this.data = data;
+    }
+}
+
+export function defaultConfig(): Config {
+    return new Config({
+        developers: []
+    });
+}
+
+export function getConfig(): Config {
+    if (!fs.existsSync(CONFIG_PATH)) {
+        return defaultConfig();
+    }
+
+    try {
+        const text = fs.readFileSync(CONFIG_PATH).toString();
+        const res = JSON.parse(text) as ConfigData;
+        
+        return new Config(res);
+    } catch (e) {
+        console.error(`Failed to read config from ${CONFIG_PATH}, using default config.\n${e}`);
+        return defaultConfig();
+    }
+}
