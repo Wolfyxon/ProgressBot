16e392084f50dd92861fe623c8aaafae5873c44e
Wolfyxon <wolfyxon@gmail.com>
2025-04-10 16:45:54 +0200

    Added /database command

---
+++ src/commands/database.ts
@@ -0,0 +1,71 @@
+import { SlashCommandBuilder } from "discord.js";
+import Command from "../command";
+
+export default new Command()
+    .makeDevOnly()
+    .setBuilder(
+        new SlashCommandBuilder()
+            .setName("database")
+            .setDescription("Database management commands, only for developers.")
+
+            .addSubcommand(cmd => cmd
+                .setName("run")
+                .setDescription("Runs a SQL query.")
+                
+                .addStringOption(opt => opt
+                    .setName("query")
+                    .setDescription("SQL query")
+                )
+            )
+            .addSubcommand(cmd => cmd
+                .setName("rmuser")
+                .setDescription("Removes a user from the database")
+
+                .addUserOption(opt => opt
+                    .setName("user")
+                    .setDescription("User to remove")
+                    .setRequired(true)
+                )
+                .addBooleanOption(opt => opt
+                    .setName("global")
+                    .setDescription("Should the user be removed from all servers")
+                    .setRequired(false)
+                )
+            )
+    )
+    .setRun(async (ctx) => {
+        switch(ctx.interaction.options.getSubcommand(true)) {
+            case "run": {
+                await ctx.interaction.deferReply();
+
+                const query = ctx.interaction.options.getString("query", true);
+                
+                try {
+                    const res = ctx.db.run(query);
+                    ctx.interaction.editReply(`Query successful. \`${res.value.changes}\` changes`);
+
+                } catch (e) {
+                    ctx.interaction.editReply(`SQL error: \`\`\`${e}\`\`\``);
+                }
+
+                break;
+            }
+
+            case "rmuser": {
+                const user = ctx.interaction.options.getUser("user", true);
+                const global = ctx.interaction.options.getBoolean("global", false) ?? false;
+
+                ctx.interaction.deferReply();
+
+                if(global) {
+                    ctx.db.users.removeUserFromAll(user.id);
+                    ctx.interaction.editReply("User removed successfully from all servers");
+                } else {
+                    ctx.db.users.removeUser(ctx.interaction.guildId!, user.id);
+                    ctx.interaction.editReply("User removed successfully from this server");
+                }
+
+                break;
+            }
+        }
+    });
