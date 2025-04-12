7efd1cfbc2db80a36f5363146b08fcfa047a6004
Wolfyxon <wolfyxon@gmail.com>
2025-04-12 13:31:40 +0200

    Moved DbGuild below Guilds

--- src/db/guilds.ts
+++ src/db/guilds.ts
@@ -8,6 +8,58 @@ export type RawDbGuild = {
     teacherRoleId?: string
 }
 
+export default class Guilds extends DbTable {
+    constructor(db: Database) {
+        super("Guilds", db);
+    }
+
+    public setup(): void {
+        this.db.db.exec(`
+            CREATE TABLE IF NOT EXISTS ${this.name} (
+                guildId VARCHAR(20) NOT NULL UNIQUE,
+                language VARCHAR(5) DEFAULT "en",
+                teacherRoleId VARCHAR(20)
+            )
+        `);
+    }
+
+    public queryGuild(guildId: string): DbResult<DbGuild | null> {
+        const res = this.db.query(`SELECT * FROM ${this.name} WHERE guildId = ?`, guildId);
+
+        if(!res.value) return res;
+
+        return new DbResult(
+            res.statement,
+            new DbGuild(res.value as RawDbGuild, this)
+        );
+    }
+
+    public queryOrSetupGuild(guildId: string): { guild: DbGuild, result: DbResult<DbGuild> | DbRunResult } {
+        const setup = this.setupGuild(guildId);
+
+        if(setup.hasChanges()) {
+            return {
+                guild: new DbGuild({
+                    guildId: guildId,
+                    language: "en"
+                }, this),
+                result: setup
+            }
+        }
+
+        const query = this.queryGuild(guildId);
+
+        return {
+            guild: query.value as DbGuild,
+            result: query as DbResult<DbGuild>
+        };
+    }
+
+    public setupGuild(guildId: string): DbRunResult {
+        return this.db.run(`INSERT OR IGNORE INTO ${this.name} (guildId) VALUES (?)`, guildId);
+    }
+}
+
 export class DbGuild {
     tbl: Guilds;
     guildId: string = "";
@@ -64,55 +116,3 @@ export class DbGuild {
         , id, this.guildId);
     }
 } 
-
-export default class Guilds extends DbTable {
-    constructor(db: Database) {
-        super("Guilds", db);
-    }
-
-    public setup(): void {
-        this.db.db.exec(`
-            CREATE TABLE IF NOT EXISTS ${this.name} (
-                guildId VARCHAR(20) NOT NULL UNIQUE,
-                language VARCHAR(5) DEFAULT "en",
-                teacherRoleId VARCHAR(20)
-            )
-        `);
-    }
-
-    public queryGuild(guildId: string): DbResult<DbGuild | null> {
-        const res = this.db.query(`SELECT * FROM ${this.name} WHERE guildId = ?`, guildId);
-
-        if(!res.value) return res;
-
-        return new DbResult(
-            res.statement,
-            new DbGuild(res.value as RawDbGuild, this)
-        );
-    }
-
-    public queryOrSetupGuild(guildId: string): { guild: DbGuild, result: DbResult<DbGuild> | DbRunResult } {
-        const setup = this.setupGuild(guildId);
-
-        if(setup.hasChanges()) {
-            return {
-                guild: new DbGuild({
-                    guildId: guildId,
-                    language: "en"
-                }, this),
-                result: setup
-            }
-        }
-
-        const query = this.queryGuild(guildId);
-
-        return {
-            guild: query.value as DbGuild,
-            result: query as DbResult<DbGuild>
-        };
-    }
-
-    public setupGuild(guildId: string): DbRunResult {
-        return this.db.run(`INSERT OR IGNORE INTO ${this.name} (guildId) VALUES (?)`, guildId);
-    }
-}
