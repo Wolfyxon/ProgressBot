4095d689ab9ffb8369aa96ced6f4833119a89c9a
Wolfyxon <wolfyxon@gmail.com>
2025-04-15 21:17:50 +0200

    Added getChannel()

--- src/botContext.ts
+++ src/botContext.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Channel, Client, TextChannel } from "discord.js";
 import Database from "./db/database";
 import { Config } from "./config";
 
@@ -6,4 +6,12 @@ export default class BotContext {
     config?: Config;
     client?: Client;
     db?: Database;
+
+    public async getChannel(id: string): Promise<Channel | null> {
+        try {
+            return await this.client!.channels.fetch(id);
+        } catch {
+            return new Promise(res => res(null));
+        }
+    }
 }
