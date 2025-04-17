5bd943625c784c0818719ff08768f5c23a00ee50
Wolfyxon <wolfyxon@gmail.com>
2025-04-17 11:45:08 +0200

    Added getMessageInChannel()

--- src/botContext.ts
+++ src/botContext.ts
@@ -1,4 +1,4 @@
-import { Channel, Client, TextChannel } from "discord.js";
+import { Channel, Client, Message, TextChannel } from "discord.js";
 import Database from "./db/database";
 import { Config } from "./config";
 
@@ -10,6 +10,14 @@ export default class BotContext {
     public async getChannel(id: string): Promise<Channel | null> {
         try {
             return await this.client!.channels.fetch(id);
+        } catch {
+            return new Promise(res => res(null));
+        }
+    }
+
+    public async getMessageInChannel(channel: TextChannel, id: string): Promise<Message | null> {
+        try {
+            return await channel.messages.fetch(id);
         } catch {
             return new Promise(res => res(null));
         }
